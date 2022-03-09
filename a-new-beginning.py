a6de8eccb33e041815b86611b19e4d58b6a80118
Daniel Kilgallon <danielkilgallon2@gmail.com>
2022-03-08 21:58:58 -0500

    adding prediction

--- a-new-beginning.py
+++ a-new-beginning.py
@@ -19,37 +19,38 @@ solutions = np.array([float(each.split('\\')[-1].split('.')[0]) for each in ims]
 # solutions = np.array([float(each.split('/')[-1].split('~')[0]) for each in ims]).astype(np.float32)
 
 model = tf.keras.Sequential([
-  tf.keras.layers.Conv2D(input_shape=(25, 25, 1), filters=4, kernel_size=15, activation='relu'),
-  # tf.keras.layers.Conv2D(filters=4, kernel_size=15, activation='relu'),
-  # tf.keras.layers.AveragePooling2D(),
-  # tf.keras.layers.Conv2D(filters=4, kernel_size=15, activation='relu'),
-  # tf.keras.layers.Conv2D(filters=4, kernel_size=15, activation='relu'),
-  # tf.keras.layers.AveragePooling2D(),
-  # tf.keras.layers.Conv2D(filters=4, kernel_size=15, activation='relu'),
-  # tf.keras.layers.Conv2D(filters=4, kernel_size=15, activation='relu'),
-  # tf.keras.layers.Flatten(),
-  # tf.keras.layers.Dense(units=512, activation='relu'),
-  # tf.keras.layers.Dense(units=256, activation='relu'),
+  tf.keras.layers.Conv2D(input_shape=(25, 25, 1), filters=4, kernel_size=2, activation='relu'),
+  tf.keras.layers.Conv2D(filters=4, kernel_size=2, activation='relu'),
+  tf.keras.layers.AveragePooling2D(),
+  tf.keras.layers.Conv2D(filters=4, kernel_size=2, activation='relu'),
+  tf.keras.layers.Conv2D(filters=4, kernel_size=2, activation='relu'),
+  tf.keras.layers.AveragePooling2D(),
+  tf.keras.layers.Conv2D(filters=4, kernel_size=2, activation='relu'),
+  tf.keras.layers.Conv2D(filters=4, kernel_size=2, activation='relu'),
+  tf.keras.layers.Flatten(),
+  tf.keras.layers.Dense(units=512, activation='relu'),
+  tf.keras.layers.Dense(units=256, activation='relu'),
   tf.keras.layers.Dense(units=64, activation='relu'),
   tf.keras.layers.Dense(units=1)
 ])
 
+model.compile(loss='mean_squared_error', optimizer="adam", metrics=['accuracy'])
 
-model.compile(loss='mean_squared_error', optimizer="adam")
+history = model.fit(questions, solutions, epochs=3, batch_size=100, verbose=1)
 
-# model.compile(
-#     optimizer='adam',
-#     loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-#     metrics=['accuracy']
-# )
+# Test model
 
-history = model.fit(questions, solutions, epochs=1, batch_size=100, verbose=1)
-"""
-test_answers = model.predict(test_questions)
+prediction_pngs = glob.glob('prediction-images/*.png')
 
+prediction_ims = {}
+for png in prediction_pngs:
+  prediction_ims[png] = np.array(PIL.Image.open(png))
 
+prediction_questions = np.array([each for each in prediction_ims.values()]).astype(np.float32)
 
-print(pngs[0])
-print(questions[0])
-print(solutions[0])
-# """
+answers = model.predict(prediction_questions)
+
+for i in range(len(answers)):
+  print('\ncard: {}'.format(prediction_pngs[i].split('\\')[-1].split('~')[-1].split('.')[0]))
+  print('prediction: ${}'.format(round(float(answers[i][0]), 2)))
+  print('actual: ${}'.format(prediction_pngs[i].split('\\')[-1].split('~')[0]))
